6f59cedc1fbf4a77876f1a219620b7c50d2ba872
Bakhshandeh Ardestani, Ali (TPD) <bakhshandeh.ali@tapsi.doctor>
2025-09-29 10:16:25 +0330

    feat: added cors origin

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
+import os
 from fastapi import FastAPI
 from app.api import upload
+from starlette.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="PDF Sanitizer Service")
+
+ENV = os.getenv("ENV", "development")
+
+allowed_origins = [
+    "https://url.com",
+
+]
+if ENV == "development":
+    allowed_origins.extend([
+        "http://localhost",
+        "http://localhost:8000",
+        "http://localhost:3000",
+        "http://localhost:4000",
+        "http://localhost:4100",
+    ])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload.router, prefix="/api/v1")
 
